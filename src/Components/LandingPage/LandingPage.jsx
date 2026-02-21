ebc26bbf8577db9f2fd9d168ac9513d5446066e7
ramez-mneizel <ramizbashar15@gmail.com>
2026-02-21 03:12:03 +0300

    1

---
+++ src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,18 @@
+//this page is the first page should run when start the project
+
+import NavBar from "../NavBar/NavBar"
+
+function LandingPage(){
+
+
+return (
+
+<>
+
+
+</>
+
+);
+
+}
+export default LandingPage;
